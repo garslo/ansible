cb4c078336cf16c77b4ea6fa65c74d2a9f9e941c
Gary Slopsema <gslopsema@gmail.com>
2018-09-20 13:28:02 -0400

    simpler way to call playbook

--- example-invocation.sh
+++ example-invocation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ansible-playbook --limit localhost -i hosts --ask-vault-pass llnwdev.yaml
+#ansible-playbook --limit localhost -i hosts --ask-vault-pass llnwdev.yaml
+ansible-playbook -i "localhost," --ask-vault-pass llnwdev.yaml
