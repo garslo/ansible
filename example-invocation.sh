9ea604c2581169dd9d4611b4f435634b879d9743
Gary Slopsema <gslopsema@Garys-MacBook-Pro.local>
2019-12-20 12:19:34 -0500

    add some common apps for osx

--- example-invocation.sh
+++ example-invocation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 #ansible-playbook --limit localhost -i hosts --ask-vault-pass llnwdev.yaml
-ansible-playbook -i "localhost," --ask-vault-pass llnwdev.yaml
+# ansible-playbook -i "localhost," --ask-vault-pass llnwdev.yaml
+# ansible-playbook -i "localhost," --connection local osx.yaml
